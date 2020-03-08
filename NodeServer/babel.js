c4dd742d6dea3e9eea429f7e41598b76796be00b
Vishal2-Hexaware <vishal2@hexaware.com>
2020-03-08 13:14:11 +0530

    pushing cde

--- NodeServer/babel.js
+++
@@ -1,7 +0,0 @@
-require("babel-polyfill")
-require("@babel/register")({
-    presets: ["@babel/preset-env"],
-  });
-  
-  // Import the rest of our application.
-  module.exports = require('./nodeServer.js')
